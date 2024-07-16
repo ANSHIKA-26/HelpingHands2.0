0e41ad40e1001202574c95cc8f20d7621ac06845
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-07-16 21:09:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,39 +10,8 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-
+import Register from "./components/register/Register"
 export default function MyApp() {
-  // State variables for each input field
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  // Handle form submission
-  // Handle form submission
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
-
-    try {
-      const response = await fetch("http://localhost:3000/api/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email, password }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      console.log("Success:", data);
-      // Here, you can handle the response further, e.g., showing a success message or redirecting the user
-    } catch (error) {
-      console.error("Error:", error);
-      // Here, you can handle errors, e.g., showing an error message to the user
-    }
-  };
 
   return (
     <div>
@@ -50,46 +19,12 @@ export default function MyApp() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register/>}/>
           {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </Router>
-
-      <h1>Welcome to my app</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
     </div>
   );
 }
